35354266b0d12a365528bbc1679f80500180334a
manup-bhat <manupbhat2004@gmail.com>
2026-04-12 22:41:07 +0530

    Delete everything in dev branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# volume_algo_trading
-This is backend using FastAPI for algo trading using volume spike scanner 
--- app/store.py
+++
@@ -1,7 +0,0 @@
-from collections import defaultdict, deque
-
-# last 500 candles per stock
-candle_store = defaultdict(lambda: deque(maxlen=500))
-
-# latest signals
-signals = []
